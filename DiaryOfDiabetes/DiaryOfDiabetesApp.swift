f17343f78f6b97700c38993ba9cfc771b70415c4
Alexey <alexey.zhukovich@gmail.com>
2024-04-25 11:13:21 +0300

    Initial Commit

---
+++ DiaryOfDiabetes/DiaryOfDiabetesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryOfDiabetesApp.swift
+//  DiaryOfDiabetes
+//
+//  Created by Alexey Zhukovich on 25.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiaryOfDiabetesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
